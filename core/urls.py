bc183f06a0f29b85720d1c2715b57e17c9dc2179
Fortunatus Adegoke <fortunatusoluwadara@gmail.com>
2024-01-15 20:53:43 +0100

    Adding landing, register, and login page

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path
-from home.views import test
+from django.urls import path, include
+# from home.views import test
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('test/', test)
+    path('', include('home.urls')),
 ]
--- home/views.py
+++ home/views.py
@@ -3,5 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 
 
+def homePage(request):
+    return render(request, 'assessor/home.html')
+
+
+def register(request):
+    return render(request, 'assessor/register.html')
+
+
+def login(request):
+    return render(request, 'assessor/login.html')
+
+
 def test(request):
     return render(request, template_name="test.html")
