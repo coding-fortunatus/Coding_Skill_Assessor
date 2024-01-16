5167af093e45a5db71bd15f713acc7f25a7fe6eb
Ramon Rasheed <ramonrash2@gmail.com>
2024-01-16 13:45:30 +0100

    Implemented Student Page and Test Page

--- home/urls.py
+++ home/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.homePage, name='home'),
     path('register', views.register, name='register'),
     path('login', views.login_view, name='login'),
-    path('test', views.test, name='test')
+    path("logout", views.logout_view, name="logout"),
+    path('test', views.test, name='test'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
