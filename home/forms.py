e2888c1f416f90dd4e5ef8e1b7e9a006a32943b0
Ramon Rasheed <ramonrash2@gmail.com>
2024-01-16 08:08:10 +0100

    Implemeted Auth

---
+++ home/forms.py
@@ -0,0 +1,41 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField()
+    name = forms.CharField(max_length=15)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['email'].widget.attrs['class'] = 'form-control'
+        self.fields['name'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
+        # Add other fields as needed
+
+    class Meta:
+        model = User
+        fields = ["name", "username",  "email", "password1", "password2"]
+
+    def save(self, commit=True):
+        user = super(RegisterForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        name = self.cleaned_data['name'].split(" ")
+        user.first_name = name[0]
+        user.last_name = name[1]
+        if commit:
+            user.save()
+        return user
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(label='Username', max_length=15)
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password'].widget.attrs['class'] = 'form-control'
